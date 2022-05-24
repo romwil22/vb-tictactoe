4a6bc9d5fe59645a93fd0e3ffeb3b22a8a86a3ed
Romwil <romwil.pilapil@gmail.com>
2022-05-25 02:17:56 +0800

    done UI design, naming button, panel, label, and form

---
+++ vb_tictactoe/vb_tictactoe/Form1.Designer.vb
@@ -0,0 +1,336 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.buttonClick1 = New System.Windows.Forms.Button()
+        Me.buttonClick2 = New System.Windows.Forms.Button()
+        Me.buttonClick3 = New System.Windows.Forms.Button()
+        Me.buttonClick4 = New System.Windows.Forms.Button()
+        Me.buttonClick5 = New System.Windows.Forms.Button()
+        Me.buttonClick6 = New System.Windows.Forms.Button()
+        Me.buttonClick7 = New System.Windows.Forms.Button()
+        Me.buttonClick8 = New System.Windows.Forms.Button()
+        Me.buttonClick9 = New System.Windows.Forms.Button()
+        Me.titlePanel = New System.Windows.Forms.Panel()
+        Me.titleLabel = New System.Windows.Forms.Label()
+        Me.buttonPanel = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.player1Label = New System.Windows.Forms.Label()
+        Me.player2Label = New System.Windows.Forms.Label()
+        Me.player1ScoreLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.menuPanel = New System.Windows.Forms.Panel()
+        Me.newGameButton = New System.Windows.Forms.Button()
+        Me.resetButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.titlePanel.SuspendLayout()
+        Me.buttonPanel.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.menuPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'buttonClick1
+        '
+        Me.buttonClick1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick1.Location = New System.Drawing.Point(3, 3)
+        Me.buttonClick1.Name = "buttonClick1"
+        Me.buttonClick1.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick1.TabIndex = 0
+        Me.buttonClick1.Text = "X"
+        Me.buttonClick1.UseVisualStyleBackColor = False
+        '
+        'buttonClick2
+        '
+        Me.buttonClick2.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick2.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick2.Location = New System.Drawing.Point(78, 3)
+        Me.buttonClick2.Name = "buttonClick2"
+        Me.buttonClick2.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick2.TabIndex = 1
+        Me.buttonClick2.Text = "O"
+        Me.buttonClick2.UseVisualStyleBackColor = False
+        '
+        'buttonClick3
+        '
+        Me.buttonClick3.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick3.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick3.Location = New System.Drawing.Point(153, 3)
+        Me.buttonClick3.Name = "buttonClick3"
+        Me.buttonClick3.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick3.TabIndex = 2
+        Me.buttonClick3.Text = "X"
+        Me.buttonClick3.UseVisualStyleBackColor = False
+        '
+        'buttonClick4
+        '
+        Me.buttonClick4.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick4.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick4.Location = New System.Drawing.Point(3, 65)
+        Me.buttonClick4.Name = "buttonClick4"
+        Me.buttonClick4.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick4.TabIndex = 0
+        Me.buttonClick4.Text = "X"
+        Me.buttonClick4.UseVisualStyleBackColor = False
+        '
+        'buttonClick5
+        '
+        Me.buttonClick5.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick5.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick5.Location = New System.Drawing.Point(78, 65)
+        Me.buttonClick5.Name = "buttonClick5"
+        Me.buttonClick5.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick5.TabIndex = 1
+        Me.buttonClick5.Text = "O"
+        Me.buttonClick5.UseVisualStyleBackColor = False
+        '
+        'buttonClick6
+        '
+        Me.buttonClick6.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick6.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick6.Location = New System.Drawing.Point(153, 65)
+        Me.buttonClick6.Name = "buttonClick6"
+        Me.buttonClick6.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick6.TabIndex = 2
+        Me.buttonClick6.Text = "X"
+        Me.buttonClick6.UseVisualStyleBackColor = False
+        '
+        'buttonClick7
+        '
+        Me.buttonClick7.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick7.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick7.Location = New System.Drawing.Point(3, 127)
+        Me.buttonClick7.Name = "buttonClick7"
+        Me.buttonClick7.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick7.TabIndex = 0
+        Me.buttonClick7.Text = "X"
+        Me.buttonClick7.UseVisualStyleBackColor = False
+        '
+        'buttonClick8
+        '
+        Me.buttonClick8.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick8.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick8.Location = New System.Drawing.Point(78, 127)
+        Me.buttonClick8.Name = "buttonClick8"
+        Me.buttonClick8.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick8.TabIndex = 1
+        Me.buttonClick8.Text = "O"
+        Me.buttonClick8.UseVisualStyleBackColor = False
+        '
+        'buttonClick9
+        '
+        Me.buttonClick9.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.buttonClick9.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonClick9.Location = New System.Drawing.Point(153, 127)
+        Me.buttonClick9.Name = "buttonClick9"
+        Me.buttonClick9.Size = New System.Drawing.Size(69, 56)
+        Me.buttonClick9.TabIndex = 2
+        Me.buttonClick9.Text = "X"
+        Me.buttonClick9.UseVisualStyleBackColor = False
+        '
+        'titlePanel
+        '
+        Me.titlePanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.titlePanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.titlePanel.Controls.Add(Me.titleLabel)
+        Me.titlePanel.Location = New System.Drawing.Point(12, 12)
+        Me.titlePanel.Name = "titlePanel"
+        Me.titlePanel.Size = New System.Drawing.Size(357, 43)
+        Me.titlePanel.TabIndex = 3
+        '
+        'titleLabel
+        '
+        Me.titleLabel.AutoSize = True
+        Me.titleLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.titleLabel.Location = New System.Drawing.Point(95, 4)
+        Me.titleLabel.Name = "titleLabel"
+        Me.titleLabel.Size = New System.Drawing.Size(169, 31)
+        Me.titleLabel.TabIndex = 0
+        Me.titleLabel.Text = "Tic Tac Toe"
+        '
+        'buttonPanel
+        '
+        Me.buttonPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.buttonPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.buttonPanel.Controls.Add(Me.buttonClick1)
+        Me.buttonPanel.Controls.Add(Me.buttonClick4)
+        Me.buttonPanel.Controls.Add(Me.buttonClick9)
+        Me.buttonPanel.Controls.Add(Me.buttonClick7)
+        Me.buttonPanel.Controls.Add(Me.buttonClick6)
+        Me.buttonPanel.Controls.Add(Me.buttonClick2)
+        Me.buttonPanel.Controls.Add(Me.buttonClick3)
+        Me.buttonPanel.Controls.Add(Me.buttonClick5)
+        Me.buttonPanel.Controls.Add(Me.buttonClick8)
+        Me.buttonPanel.Location = New System.Drawing.Point(12, 79)
+        Me.buttonPanel.Name = "buttonPanel"
+        Me.buttonPanel.Size = New System.Drawing.Size(230, 193)
+        Me.buttonPanel.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.player1ScoreLabel)
+        Me.GroupBox1.Controls.Add(Me.player2Label)
+        Me.GroupBox1.Controls.Add(Me.player1Label)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(248, 61)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(121, 85)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Score Board:"
+        '
+        'player1Label
+        '
+        Me.player1Label.AutoSize = True
+        Me.player1Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.player1Label.Location = New System.Drawing.Point(7, 21)
+        Me.player1Label.Name = "player1Label"
+        Me.player1Label.Size = New System.Drawing.Size(74, 18)
+        Me.player1Label.TabIndex = 0
+        Me.player1Label.Text = "Player 1:"
+        '
+        'player2Label
+        '
+        Me.player2Label.AutoSize = True
+        Me.player2Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.player2Label.Location = New System.Drawing.Point(7, 55)
+        Me.player2Label.Name = "player2Label"
+        Me.player2Label.Size = New System.Drawing.Size(74, 18)
+        Me.player2Label.TabIndex = 1
+        Me.player2Label.Text = "Player 2:"
+        '
+        'player1ScoreLabel
+        '
+        Me.player1ScoreLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.player1ScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.player1ScoreLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.player1ScoreLabel.Location = New System.Drawing.Point(81, 18)
+        Me.player1ScoreLabel.Name = "player1ScoreLabel"
+        Me.player1ScoreLabel.Size = New System.Drawing.Size(25, 26)
+        Me.player1ScoreLabel.TabIndex = 2
+        Me.player1ScoreLabel.Text = "0"
+        Me.player1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(81, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 26)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "0"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'menuPanel
+        '
+        Me.menuPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.menuPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.menuPanel.Controls.Add(Me.exitButton)
+        Me.menuPanel.Controls.Add(Me.resetButton)
+        Me.menuPanel.Controls.Add(Me.newGameButton)
+        Me.menuPanel.Location = New System.Drawing.Point(249, 153)
+        Me.menuPanel.Name = "menuPanel"
+        Me.menuPanel.Size = New System.Drawing.Size(120, 132)
+        Me.menuPanel.TabIndex = 6
+        '
+        'newGameButton
+        '
+        Me.newGameButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.newGameButton.Location = New System.Drawing.Point(8, 5)
+        Me.newGameButton.Name = "newGameButton"
+        Me.newGameButton.Size = New System.Drawing.Size(102, 36)
+        Me.newGameButton.TabIndex = 0
+        Me.newGameButton.Text = "New Game"
+        Me.newGameButton.UseVisualStyleBackColor = True
+        '
+        'resetButton
+        '
+        Me.resetButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.resetButton.Location = New System.Drawing.Point(8, 47)
+        Me.resetButton.Name = "resetButton"
+        Me.resetButton.Size = New System.Drawing.Size(102, 36)
+        Me.resetButton.TabIndex = 0
+        Me.resetButton.Text = "Reset"
+        Me.resetButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitButton.Location = New System.Drawing.Point(7, 89)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(102, 36)
+        Me.exitButton.TabIndex = 0
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(381, 297)
+        Me.Controls.Add(Me.menuPanel)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.buttonPanel)
+        Me.Controls.Add(Me.titlePanel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TIC TAC TOE"
+        Me.titlePanel.ResumeLayout(False)
+        Me.titlePanel.PerformLayout()
+        Me.buttonPanel.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.menuPanel.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents buttonClick1 As Button
+    Friend WithEvents buttonClick2 As Button
+    Friend WithEvents buttonClick3 As Button
+    Friend WithEvents buttonClick4 As Button
+    Friend WithEvents buttonClick5 As Button
+    Friend WithEvents buttonClick6 As Button
+    Friend WithEvents buttonClick7 As Button
+    Friend WithEvents buttonClick8 As Button
+    Friend WithEvents buttonClick9 As Button
+    Friend WithEvents titlePanel As Panel
+    Friend WithEvents titleLabel As Label
+    Friend WithEvents buttonPanel As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents player1ScoreLabel As Label
+    Friend WithEvents player2Label As Label
+    Friend WithEvents player1Label As Label
+    Friend WithEvents menuPanel As Panel
+    Friend WithEvents exitButton As Button
+    Friend WithEvents resetButton As Button
+    Friend WithEvents newGameButton As Button
+End Class
